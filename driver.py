bec698b4b4eff233a290c0d6b57e2614aa8d4e50
Samridhi98 <39233679+Samridhi98@users.noreply.github.com>
2020-05-25 13:17:33 +0530

    Final scripts

--- driver.py
+++ driver.py
@@ -2,7 +2,7 @@ from using_url_wiki import url_driver
 from using_doc import doc_driver
 from using_pdf import pdf_driver
 from using_social import social_driver
-print("Select the required input source type: ")
+print("Select the input source type: ")
 print("1. URL or Wikipedia")
 print("2. Text document")
 print("3. PDF")
